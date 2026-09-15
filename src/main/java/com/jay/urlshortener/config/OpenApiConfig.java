94df17559715e7c358c3f85399ebff8326e1ac39
Jayesh Pandharkar <jay.visions6859@gmail.com>
2026-09-16 00:12:23 +0530

    Add JWT authentication to Swagger

---
+++ src/main/java/com/jay/urlshortener/config/OpenApiConfig.java
@@ -0,0 +1,32 @@
+package com.jay.urlshortener.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+
+        return new OpenAPI()
+                .info(new Info()
+                        .title("URL Shortener API")
+                        .version("1.0")
+                        .description("REST API for creating, managing, and redirecting shortened URLs."))
+                .components(
+                        new Components()
+                                .addSecuritySchemes(
+                                        "bearerAuth",
+                                        new SecurityScheme()
+                                                .type(SecurityScheme.Type.HTTP)
+                                                .scheme("bearer")
+                                                .bearerFormat("JWT")
+                                )
+                );
+    }
+}
--- src/main/java/com/jay/urlshortener/controller/ShortUrlController.java
+++ src/main/java/com/jay/urlshortener/controller/ShortUrlController.java
@@ -9,11 +9,12 @@ import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
-
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/urls")
+@SecurityRequirement(name = "bearerAuth")
 public class ShortUrlController {
 
     private final ShortUrlService shortUrlService;
